ac1ab87de7b08214552b9fdf9a73879cd5e9c228
htmltest <struev.vs@gmail.com>
2018-08-01 10:10:31 +0300

    main+event

---
+++ js/tools.js
@@ -0,0 +1,60 @@
+$(document).ready(function() {
+
+    $('.main-anonces-list-inner').slick({
+        dots: false,
+        infinite: false,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        prevArrow: '<button type="button" class="slick-prev"></button>',
+        nextArrow: '<button type="button" class="slick-next"></button>',
+        responsive: [
+            {
+                breakpoint: 1169,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+
+    $(window).on('load resize', function() {
+
+        $('.main-anonces-list-inner').each(function() {
+            var curList = $(this);
+
+            curList.find('.main-anonces-item-preview').css({'min-height': '0px'});
+            var previewMax = 0;
+
+            curList.find('.main-anonces-item-preview').each(function() {
+                var curBlock = $(this);
+                var curHeight = curBlock.height();
+                if (curHeight > previewMax) {
+                    previewMax = curHeight;
+                }
+            });
+            curList.find('.main-anonces-item-title').css({'min-height': previewMax + 'px'});
+
+            curList.find('.main-anonces-item-title').css({'min-height': '0px'});
+            var titleMax = 0;
+
+            curList.find('.main-anonces-item-title').each(function() {
+                var curBlock = $(this);
+                var curHeight = curBlock.height();
+                if (curHeight > titleMax) {
+                    titleMax = curHeight;
+                }
+            });
+            curList.find('.main-anonces-item-title').css({'min-height': titleMax + 'px'});
+        });
+
+    });
+
+});
