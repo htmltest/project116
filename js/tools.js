b2bb543361edf0fb73e35cf8686f61af87e1da78
htmltest <struev.vs@gmail.com>
2018-08-08 15:50:12 +0300

    typo, profit, webinars

--- js/tools.js
+++ js/tools.js
@@ -57,7 +57,6 @@ $(document).ready(function() {
 
     });
 
-
     if ($('.events-filter-years').length > 0) {
         var eventsYear = $('.events-filter-years input').index($('.events-filter-years input:checked'));
 
@@ -105,4 +104,177 @@ $(document).ready(function() {
         e.preventDefault();
     });
 
+    $('.gallery').slick({
+        dots: true,
+        infinite: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        prevArrow: '<button type="button" class="slick-prev"></button>',
+        nextArrow: '<button type="button" class="slick-next"></button>'
+    });
+
+    $('.gallery-item a').fancybox({
+        buttons : [
+            'close'
+        ],
+        lang : 'ru',
+        i18n : {
+            'ru' : {
+                DOWNLOAD    : 'Скачать',
+                CLOSE       : 'Закрыть',
+                NEXT        : 'Вперед',
+                PREV        : 'Назад'
+            }
+        }
+    });
+
+    $.validator.addMethod('maskPhone',
+        function(value, element) {
+            if (value == '') {
+                return true;
+            }
+            return /^\+7 \(\d{3}\) \d{3}\-\d{2}\-\d{2}$/.test(value);
+        },
+        'Не соответствует формату'
+    );
+
+    $('form').each(function() {
+        initForm($(this));
+    });
+
+    $('.webinar-header-link').click(function(e) {
+        $('html, body').animate({'scrollTop': $('.webinars-forms').offset().top - 24});
+        $('.webinars-forms-menu li:first a').trigger('click');
+        e.preventDefault();
+    });
+
+    $('.webinars-forms-menu li a').click(function(e) {
+        var curLi = $(this).parent();
+        $('.webinars-form').stop(true, true);
+        if (curLi.hasClass('active')) {
+            curLi.removeClass('active');
+            $('.webinars-form:visible').fadeOut(200);
+        } else {
+            $('.webinars-forms-menu li.active').removeClass('active');
+            curLi.addClass('active');
+            var curIndex = $('.webinars-forms-menu li').index(curLi);
+            if ($('.webinars-form:visible').length > 0) {
+                $('.webinars-form:visible').fadeOut(200, function() {
+                    $('.webinars-form').eq(curIndex).fadeIn(200);
+                });
+            } else {
+                $('.webinars-form').eq(curIndex).fadeIn(200);
+            }
+        }
+        e.preventDefault();
+    });
+
+    $('.webinars-form-all').click(function(e) {
+        $('.webinars-form-items input').prop('checked', true).trigger('change');
+        e.preventDefault();
+    });
+
+    $('.webinars-form-clear').click(function(e) {
+        $('.webinars-form-items input').prop('checked', false).trigger('change');
+        e.preventDefault();
+    });
+
+    $('.webinars-form-items input').change(function() {
+        recalcWebinars();
+    });
+
+    recalcWebinars();
+
+    $('.profit-header-text').click(function(e) {
+        $(this).parents().filter('.profit').toggleClass('open');
+        e.preventDefault();
+    });
+
 });
+
+$(window).on('resize', function() {
+    $('.form-select select').chosen('destroy');
+    $('.form-select select').chosen({disable_search: true, placeholder_text_multiple: ' ', no_results_text: 'Нет результатов'});
+});
+
+function initForm(curForm) {
+    curForm.find('input.maskPhone').mask('+7 (999) 999-99-99');
+
+    curForm.find('.form-select select').chosen({disable_search: true, no_results_text: 'Нет результатов'});
+
+    curForm.find('.form-file input').change(function() {
+        var curInput = $(this);
+        var curField = curInput.parent().parent().parent().parent();
+        curField.find('.form-file-name-text').html(curInput.val().replace(/.*(\/|\\)/, ''));
+        curField.find('label.error').remove();
+        curField.removeClass('error');
+    });
+
+    curForm.find('.form-file-name-remove').click(function() {
+        var curField = $(this).parents().filter('.form-file');
+        curField.find('.form-file-name-text').html('');
+        curField.find('input').val(null);
+    });
+
+    curForm.validate({
+        ignore: '',
+        invalidHandler: function(form, validatorcalc) {
+            validatorcalc.showErrors();
+            checkErrors();
+        }
+    });
+}
+
+function checkErrors() {
+    $('.form-input').each(function() {
+        var curField = $(this).parent().parent();
+        if (curField.find('input.error').length > 0 || curField.find('textarea.error').length > 0) {
+            curField.addClass('error');
+        } else {
+            curField.removeClass('error');
+        }
+        if (curField.find('input.valid').length > 0 || curField.find('textarea.valid').length > 0) {
+            curField.addClass('valid');
+        } else {
+            curField.removeClass('valid');
+        }
+    });
+
+    $('.form-checkbox, .form-file').each(function() {
+        var curField = $(this);
+        if (curField.find('input.error').length > 0) {
+            curField.addClass('error');
+        } else {
+            curField.removeClass('error');
+        }
+        if (curField.find('input.valid').length > 0) {
+            curField.addClass('valid');
+        } else {
+            curField.removeClass('valid');
+        }
+    });
+
+    $('.form-select').each(function() {
+        var curField = $(this).parent().parent();
+        if (curField.find('select.error').length > 0) {
+            curField.addClass('error');
+        } else {
+            curField.removeClass('error');
+        }
+        if (curField.find('select.valid').length > 0) {
+            curField.addClass('valid');
+        } else {
+            curField.removeClass('valid');
+        }
+    });
+}
+
+function recalcWebinars() {
+    var curCount = $('.webinars-form-items input:checked').length;
+    var curCost = 0;
+    $('.webinars-form-items input:checked').each(function() {
+        curCost += Number($(this).data('price'));
+    });
+    $('.webinars-form-summ-count').html(curCount);
+    $('.webinars-form-summ-cost').html(curCost);
+}
